bffaede56564ef0ae2ce70e0c2bc47f2f42d9f73
gadget114514 <gadget114514@gmail.com>
2019-03-09 23:10:09 +0900

    initial commit

---
+++ proj.linux/CMakeFiles/TemplateCpp.dir/DependInfo.cmake
@@ -0,0 +1,84 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/opt/local/src/cocos2dx/adv/Classes/AppDelegate.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/AppDelegate.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/AudioHelper.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/AudioHelper.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/CharLabel.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/CharLabel.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/Control/EasyButton.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/Control/EasyButton.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/Control/PopupLayer.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/Control/PopupLayer.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/Control/RadioButton.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/Control/RadioButton.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/Control/Slidebar.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/Control/Slidebar.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/GameScene.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/GameScene.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/GameSystem.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/GameSystem.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/HelloWorldScene.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/HelloWorldScene.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/History.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/History.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/HistoryScene.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/HistoryScene.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/LoadScene.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/LoadScene.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/MainMenuScene.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/MainMenuScene.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/SaveData.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/SaveData.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/SaveScene.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/SaveScene.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/BackgroundManager.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/BackgroundManager.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/BackgroundMusicManager.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/BackgroundMusicManager.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/CharacterManager.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/CharacterManager.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCBackground.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCBackground.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCCharacterSpeak.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCCharacterSpeak.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCGameOver.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCGameOver.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCIf.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCIf.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCJump.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCJump.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCLeave.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCLeave.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCPlayBGM.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCPlayBGM.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCPlaySound.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCPlaySound.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCSelect.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCSelect.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCSet.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCSet.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCShowFg.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCShowFg.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCStopBGM.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCStopBGM.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SCStopSound.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SCStopSound.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/ScriptCommand.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/ScriptCommand.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/ScriptReader.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/ScriptReader.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/ScriptReader/SoundManager.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/ScriptReader/SoundManager.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/SettingScene.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/SettingScene.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/TestFolder/TestInFolder.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/TestFolder/TestInFolder.cpp.o"
+  "/opt/local/src/cocos2dx/adv/Classes/TestLayer.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/Classes/TestLayer.cpp.o"
+  "/opt/local/src/cocos2dx/adv/proj.linux/main.cpp" "/opt/local/src/cocos2dx/adv/proj.linux/CMakeFiles/TemplateCpp.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CC_USE_JPEG=1"
+  "CC_USE_NAVMESH=1"
+  "CC_USE_PNG=1"
+  "CC_USE_TIFF=1"
+  "COCOS2D_DEBUG=1"
+  "LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../cocos2d"
+  "../cocos2d/cocos"
+  "../cocos2d/deprecated"
+  "../cocos2d/cocos/platform"
+  "../cocos2d/extensions"
+  "../cocos2d/external"
+  "../cocos2d/external/glfw3/include/linux"
+  "../Classes"
+  "../cocos2d/cocos/audio/include"
+  "/../yasl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/opt/local/src/cocos2dx/adv/proj.linux/engine/cocos/core/CMakeFiles/cocos2d.dir/DependInfo.cmake"
+  "/opt/local/src/cocos2dx/adv/proj.linux/engine/external/unzip/CMakeFiles/unzip.dir/DependInfo.cmake"
+  "/opt/local/src/cocos2dx/adv/proj.linux/engine/external/tinyxml2/CMakeFiles/tinyxml2.dir/DependInfo.cmake"
+  "/opt/local/src/cocos2dx/adv/proj.linux/engine/external/flatbuffers/CMakeFiles/flatbuffers.dir/DependInfo.cmake"
+  "/opt/local/src/cocos2dx/adv/proj.linux/engine/external/xxhash/CMakeFiles/xxhash.dir/DependInfo.cmake"
+  "/opt/local/src/cocos2dx/adv/proj.linux/engine/external/recast/CMakeFiles/recast.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
